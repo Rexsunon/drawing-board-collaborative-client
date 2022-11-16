244fa9e95abd3a944f24f1dcc058730a1f1f9ac8
Evidence Rex Osikhena <rexsunon.dev@gmail.com>
2022-11-16 18:02:33 +0100

    add: connect to socket and lisent on events

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import appPackage from '../package.json';
 import expressConfig from './config/expressConfig'; 
 import 'dotenv/config';
 import { SocketServer } from './socket';
+import { io} from 'socket.io-client';
+import { attempt } from 'lodash';
+
 // import './config/db_connection'; // uncomment for database connection
 
 
@@ -19,7 +22,49 @@ app.set('APP_PACKAGE', {
 expressConfig(app);
 
 const server = http.createServer(app);
-new SocketServer(server);
+// new SocketServer(server);
+const socket = io('ws://localhost:3000');
+
+const startListening = () => {
+  // Reconnect event
+  socket.on('reconnect', (attempt) => {
+    logger.info(`Attempting to reconnect: ${attempt}`);
+  });
+
+
+  // Reconnection attempt event
+  socket.on('reconnect_attempt', (attempt) => {
+    logger.error(`Reconnection attempt: ${attempt}`);
+  });
+
+  // Reconnection error
+  socket.on('reconnect_error', (error) => {
+    logger.error(`Reconnection error: ${error}`);
+  });
+
+  // Reconnect failed
+  socket.on('reconnect_failed', () => {
+    logger.error(`Reconnection failure`);
+  });
+}
+
+const sendHandshake = () => {
+  logger.info('Sending handshake to server ...');
+
+  socket.emit('handshake', (uid: string, borads: string[]) => {
+    logger.info("Board handsake callback message revceived.");
+    logger.info(`board_uid: ${uid}`);
+    logger.info(`boards: ${borads}`);
+  });
+
+  socket.emit("draw", JSON.parse(`{"points": [{"dx": 575.6796875, "dy": 515.73046875}], "color": "#000000", "size": 10, "filled": false, "type": "scribble", "sides": 3}`))
+}
+
+socket.connect();
+
+startListening();
+
+sendHandshake();
 
 server.listen(port, () => logger.info(`App listening on port ${port}...`));
 
